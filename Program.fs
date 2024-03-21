05a1c2ca977a85e2ff19f0655ec2cf1aee38edc2
bibeklm07 <160158460+bibeklm07@users.noreply.github.com>
2024-03-22 01:36:47 +0545

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello It's me Bibek!#"
+let rec factorial b acc =
+    if b = 0 then
+        acc
+    else
+        factorial (b - 1) (acc * b)
+
+let result = factorial 4 2
+printfn "Factorial of 4 is: %d" result
+
